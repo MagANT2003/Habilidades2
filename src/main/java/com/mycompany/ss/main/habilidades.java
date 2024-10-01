b7704c92a63207ab82b031f2bacfdc295d275a40
MagANT2003 <nr112173@gmail.com>
2024-10-01 13:31:16 -0600

    primer commit

--- src/main/java/com/mycompany/ss/main/habilidades.java
+++ src/main/java/com/mycompany/ss/main/habilidades.java
@@ -20,6 +20,11 @@ public class habilidades {
         correo = "nr112173@gmail.com";
         celular = "466-115-1093";
         direccion = "Maravatio del encinal calle mariano matamoros #305B";
+        lenguajes = "[ JAVA ]--" +  "[ C++ ]--" + "[ PYTON ]--" + "[ C# ]";
+        bd = "[MYSQL]--[SQLSERVER]--[ORACLE DATABASE]";
+        proyectos = "1.Creacion de un software para una tienda";
+        proyecto2 = "2.Como programar un brazo robotico KUKA";
+        calificacion = "91";
     }
     
     
@@ -28,7 +33,54 @@ public class habilidades {
     private String correo;
     private String celular;
     private String direccion;
+    private String lenguajes;
+    private String bd;
+    private String proyectos;
+    private String calificacion;
+    private String proyecto2;
 
+    public String getProyecto2() {
+        return proyecto2;
+    }
+
+    public void setProyecto2(String proyecto2) {
+        this.proyecto2 = proyecto2;
+    }
+
+    
+    public String getLenguajes() {
+        return lenguajes;
+    }
+
+    public void setLenguajes(String lenguajes) {
+        this.lenguajes = lenguajes;
+    }
+
+    public String getBd() {
+        return bd;
+    }
+
+    public void setBd(String bd) {
+        this.bd = bd;
+    }
+
+    public String getProyectos() {
+        return proyectos;
+    }
+
+    public void setProyectos(String proyectos) {
+        this.proyectos = proyectos;
+    }
+
+    public String getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(String calificacion) {
+        this.calificacion = calificacion;
+    }
+    
+    
     public String getNombre() {
         return nombre;
     }
